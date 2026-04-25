31f02fd09866a8e346dbf22885ae5f6f2ad94187
Sebastiaan Mannem <sebas@mannem.nl>
2026-04-25 10:04:22 +0200

    Improved docs

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# pgv_kvm
+# pgv_vagrant
 
-Running a demo environment of pgvillage on your laptop with kvm
+Running a demo environment of pgvillage on your laptop using vagrant
 
-Example setup is described in steps.md
+## Getting started
 
+1. Get a machine (e.a. laptop) with Ubuntu
+2. Setup KVM, create machines, etc. ([See here](./steps.md))
+3. Make sure all [preps](./preps.md) are ready
+4. Log in to ansible host and deploy `cd git/pgvillage/ && ansible-playbook -i environments/cluster1/ functional-all.yml`
+5. Be amazed
---
+++ scripts/bootstrap_rocky-9.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+echo Setup git
+ssh-keyscan -H github.com >>~/.ssh/known_hosts
+sudo dnf install -y git pkg-config openssl-devel
+if [ ! -d ~/git/pg_kvm ]; then
+	mkdir -p ~/git && cd ~/git && git clone https://github.com/pgvillage/pgv_kvm
+fi
+if [ ! -d ~/git/pgvillage ]; then
+	cd ~/git && git clone https://github.com/pgvillage/pgvillage && cd pgvillage && ln -s ~/git/pgv_kvm/environments .
+fi
+
+cd
+
+echo Install python3
+# For more info, see https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+sudo dnf install -y python3-pip libffi-devel
+pip3 install virtualenv
+. ~/.bashrc
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+if ! [ -e ~/venv ]; then
+	mkdir ~/venv
+	virtualenv ~/venv
+	. ~/venv/bin/activate
+	echo . ~/venv/bin/activate >>~/.bashrc
+fi
+
+echo Install ansible
+pip3 install --upgrade pip ansible #--upgrade cryptography packaging netaddr msgraph-sdk
+
+echo Setup ssh localhost
+[ -f ~/.ssh/id_rsa.pub ] || ssh-keygen -q -f ~/.ssh/id_rsa -P ""
+grep -q "$USER@$HOSTNAME" ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_keys
+ssh-keygen -H -F localhost >/dev/null || ssh-keyscan -H localhost >>~/.ssh/known_hosts
+
+which ansible-playbook >/dev/null || echo 'Please reload your profile to have ansible-playbook in your path (probably logout/login or `source ~/.bashrc`)'
+
+source ~/.bashrc
+ansible-galaxy role install --role-file ~/git/pgvillage/requirements.yml
+ansible-galaxy collection install --requirements-file ~/git/pgvillage/requirements.yml
